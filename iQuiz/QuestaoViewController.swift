04e503d8b7fee8c757457e369985bc3d9d61330d
Yuri Gonçalves <yuriv.goncalves@gmail.com>
2024-04-05 22:09:01 -0300

    feature: adicionada tela de questão

--- iQuiz/QuestaoViewController.swift
+++ iQuiz/QuestaoViewController.swift
@@ -9,12 +9,26 @@ import UIKit
 
 class QuestaoViewController: UIViewController {
 
+    @IBOutlet weak var tituloQuesaoLabel: UILabel!
+    
+    @IBOutlet var BotoesRespostas: [UIButton]!
+    
+    @IBAction func respostaBotaoPressionado(_ sender: UIButton) {
+        print(sender.tag)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        configurarLayout()
     }
     
+    func configurarLayout() {
+        navigationItem.hidesBackButton = true
+        
+        for botao in BotoesRespostas {
+            botao.layer.cornerRadius = 12.0
+        }
+    }
 
     /*
     // MARK: - Navigation
